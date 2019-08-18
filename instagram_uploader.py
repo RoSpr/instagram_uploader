1701120d7bff3ef54dbbd163206bb5c131f72f87
RoSpr <rsprikut@mail.ru>
2019-08-18 15:13:07 +0500

    Update instagram_uploader.py

--- instagram_uploader.py
+++ instagram_uploader.py
@@ -1,5 +1,7 @@
 import requests
 import os
+import argparse
+import urllib
 
 from PIL import Image
 from instabot import Bot
@@ -15,67 +17,43 @@ def load_picture(pic_url, pic_name):
     filename = PATH + filepath
 
     directory = os.path.dirname(filename)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    os.makedirs(directory, exist_ok=True)
 
     response = requests.get(pic_url, verify=False)
+    response.raise_for_status()
 
     with open(filename, 'wb') as file:
         file.write(response.content)
 
 
-def fetch_spacex():
+def fetch_spacex_links(launch_number):
     spacex_url = "https://api.spacexdata.com/v3/launches"
 
     response = requests.get(spacex_url)
-    links = response.json()[81]['links']['flickr_images']
+    response.raise_for_status()
 
-    for link_number, link in enumerate(links):
-        pic_name = "spacex_{}.jpg".format(link_number)
-        load_picture(link, pic_name)
+    links = response.json()[launch_number]['links']['flickr_images']
 
-        resize_img(pic_name, pic_name, 900)
-        make_black_background(pic_name, pic_name)
-
-        print("{} is downloaded".format(pic_name))
+    return links
 
 
 def get_extension(link):
-    str = link.split('.')
-    extension = str[-1]
+    link_parts = link.split('.')
+    extension = link_parts[-1]
     return extension
 
 
-def fetch_hubble(collection_name):
-    collection_url = "http://hubblesite.org/api/v3/images?page=all&collection_name={}".format(collection_name)
-    collection_response = requests.get(collection_url)
+def fetch_hubble_collection(collection_name):
+
+    url = "http://hubblesite.org/api/v3/images"
+    params = urllib.parse.urlencode({'page': "all&collection_name={}".format(collection_name)})
+
+    collection_response = requests.get(url, params=params)
+    collection_response.raise_for_status()
+
     collection_response = collection_response.json()
 
-    for pic_info in collection_response:
-        pic_id = pic_info['id']
-
-        hubble_url = "http://hubblesite.org/api/v3/image/{}".format(pic_id)
-
-        response = requests.get(hubble_url)
-        links_data = response.json()['image_files']
-
-        links = []
-        for link_data in links_data:
-            link = link_data['file_url']
-            links.append(link)
-
-        pic_url = links[-1]
-        pic_url = pic_url.replace("//", "https://")
-
-        pic_extension = get_extension(links[-1])
-        pic_name = "hubble_{}.{}".format(pic_id, pic_extension)
-
-        load_picture(pic_url, pic_name)
-
-        print("Picture with id {} is downloaded".format(pic_id))
-
-        resize_img(pic_name, pic_name, 900)
-        make_black_background(pic_name, pic_name)
+    return collection_response
 
 
 def resize_img(filename, new_filename, max_size):
@@ -98,10 +76,10 @@ def make_black_background(filename, new_filename):
     img = Image.open(PATH + "images\{}".format(filename))
     black_img = Image.open(PATH + "black.jpg")
 
-    x = int((black_img.size[0] - img.size[0])/2)
-    y = int((black_img.size[1] - img.size[1])/2)
+    x = int((black_img.size[0] - img.size[0]) / 2)
+    y = int((black_img.size[1] - img.size[1]) / 2)
 
-    black_img.paste(img,(x,y))
+    black_img.paste(img, (x, y))
     black_img.save(PATH + "images\{}".format(new_filename))
 
 
@@ -120,9 +98,61 @@ def post_instagram():
 
 
 if __name__ == "__main__":
-    print("Enter Hubble picture collection (e.g.: wallpaper, holiday_cards, "
-          "spacecraft, news, printshop, stsci_gallery)\n")
-    pic_collection = input()
-    fetch_hubble(pic_collection)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("hubble_collection_name", help="downloads photos from Hubble collection given (e.g.: wallpaper, "
+                                                       "holiday_cards, spacecraft, news, printshop, stsci_gallery)",
+                        type=str)
+    args = parser.parse_args()
+    collection_name = args.hubble_collection_name
 
-    fetch_spacex()
+    try:
+        collection_response = fetch_hubble_collection(collection_name)
+
+        for pic_info in collection_response:
+            pic_id = pic_info['id']
+
+            hubble_url = "http://hubblesite.org/api/v3/image/{}".format(pic_id)
+
+            response = requests.get(hubble_url)
+
+            links_data = response.json()['image_files']
+
+            links = [link_data['file_url'] for link_data in links_data]
+
+            pic_url = links[-1]
+            pic_url = pic_url.replace("//", "https://")
+
+            pic_extension = get_extension(links[-1])
+            pic_name = "hubble_{}.{}".format(pic_id, pic_extension)
+
+            load_picture(pic_url, pic_name)
+
+            print("Picture hubble_{}.{} is downloaded".format(pic_id, pic_extension))
+
+            resize_img(pic_name, pic_name, 900)
+            make_black_background(pic_name, pic_name)
+
+        print("All Hubble pictures from {} collection are downloaded".format(collection_name))
+
+    except requests.exceptions.HTTPError as error:
+        print("Error: {}".format(error))
+
+    try:
+        launch_number = 81 #real launch number is 82 as elements in the list start with 0
+        links = fetch_spacex_links(launch_number)
+
+        for link_number, link in enumerate(links):
+            pic_name = "spacex_{}.jpg".format(link_number)
+            load_picture(link, pic_name)
+
+            resize_img(pic_name, pic_name, 900)
+            make_black_background(pic_name, pic_name)
+
+            print("{} is downloaded".format(pic_name))
+
+        print("All SpaceX images are downloaded")
+
+    except requests.exceptions.HTTPError as error:
+        print("Error: {}".format(error))
+
+post_instagram()
